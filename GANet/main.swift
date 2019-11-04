46fd18c766d8ca55ea258873ed86e4558b119092
Alexei Shchekochikhin <ashekochikhin@gmail.com>
2019-11-04 18:18:40 +0300

    Initial Commit

---
+++ GANet/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GANet
+//
+//  Created by Aleksei Shchekochikhin on 04/11/2019.
+//  Copyright © 2019 Aleksei Shchekochikhin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
